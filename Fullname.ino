7c0899fb31061ccbf29103b835926d6696a104b0
CSI-Ricardo-Rovira <93545943+CSI-Ricardo-Rovira@users.noreply.github.com>
2022-03-10 14:32:43 -0400

    Create Fullname.ino

---
+++ Fullname.ino
@@ -0,0 +1,177 @@
+int a = 7;
+int b = 6;
+int c = 4;
+int d = 2;
+int e = 1;
+int f = 9; 
+int g = 10;
+int decimal = 5;
+
+void setup()
+{
+  pinMode (a, OUTPUT);
+  pinMode (b, OUTPUT);
+  pinMode (c, OUTPUT);
+  pinMode (d, OUTPUT);
+  pinMode (e, OUTPUT);
+  pinMode (f, OUTPUT);
+  pinMode (g, OUTPUT);
+  pinMode (decimal, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal,LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+  digitalWrite (decimal, LOW);
+
+  delay(2000); 
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal, LOW);
+
+  delay(2000); 
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, LOW);
+  digitalWrite (f, LOW);
+  digitalWrite (g, LOW);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, LOW);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, LOW);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  digitalWrite (decimal, LOW);
+
+  delay(2000);
+}
